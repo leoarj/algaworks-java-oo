f37bf5ea138bca49b3c0faae062834b47711e48f
Leandro Antônio Rocha Araújo <leandro.araujo@outlook.com>
2023-04-18 22:01:56 -0400

    docs(TestMegaSenaExample/Math/Arrays): updates the block comments.

--- src/com/algaworks/course/fjoo/math/TestMegaSenaExample.java
+++ src/com/algaworks/course/fjoo/math/TestMegaSenaExample.java
@@ -6,5 +6,18 @@ public class TestMegaSenaExample {
 		MegaSenaExample example = new MegaSenaExample(10);
 		example.sortearNumeros();
 		example.exibirJogos();
+		
+		/*
+		 * Exemplo de saída:
+		[1, 7, 10, 18, 38, 55]
+		[2, 6, 14, 23, 31, 49]
+		[1, 9, 16, 21, 23, 56]
+		[4, 20, 23, 32, 46, 50]
+		[1, 32, 43, 49, 55, 58]
+		[3, 4, 20, 30, 51, 58]
+		[15, 17, 19, 37, 38, 58]
+		[24, 26, 37, 51, 53, 54]
+		[8, 15, 18, 27, 31, 51]
+		*/
 	}	
 }
